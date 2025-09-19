5e1af3f5f51df63fa8faa5d0bff411db2756127a
Jay Mills <jay.mills@student.sl.on.ca>
2025-09-19 09:51:55 -0400

    Added initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HouseCard from './components/houseCard'
+
+function App() {
+
+  return (
+    <>
+      <HouseCard address="12345"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/houseCard.jsx
@@ -0,0 +1,5 @@
+export default function HouseCard(props){
+    return <div>
+        <p>{props.address}</p>
+    </div>
+}
